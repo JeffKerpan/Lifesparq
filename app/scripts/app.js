defe7b054b21680fa1647c4e08403a6cd308f361
Heathcliff <dennis.nathan@gmail.com>
2017-07-08 16:37:42 -0600

    Initial commit after having built basic layout of homepage.

---
+++ app/scripts/app.js
@@ -0,0 +1,39 @@
+(function() {
+
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name lifesparqApp
+ * @description
+ * # lifesparqApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('lifesparqApp', [
+    'ngMaterial',
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
+})();
