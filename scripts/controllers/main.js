fda0e07a3a1fb757bb315da5aca12115944ffee8
Heathcliff <dennis.nathan@gmail.com>
2017-07-08 16:37:42 -0600

    Initial commit after having built basic layout of homepage.

---
+++ scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lifesparqApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the lifesparqApp
+ */
+angular.module('lifesparqApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
