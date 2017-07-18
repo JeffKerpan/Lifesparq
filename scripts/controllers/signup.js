ccd7e733570cfde889708600f260f55169dae7fc
Heathcliff <dennis.nathan@gmail.com>
2017-07-18 15:51:05 -0600

    Adding a coach and team now properly updates database to connect the two.

--- scripts/controllers/signup.js
+++ scripts/controllers/signup.js
@@ -1,4 +1,46 @@
 'use strict';
 
 angular.module('lifesparqApp')
-  .controller('signUpCtrl', function ($scope, $mdSidenav, $timeout, $log) {});
+  .controller('signUpCtrl', function ($scope, $mdSidenav, $timeout, $log, $localStorage, $http) {
+    $scope.firstName = $localStorage.firstName;
+    $scope.lastName = $localStorage.lastName;
+    $scope.emailAddress = $localStorage.emailAddress;
+
+    $scope.coachAndTeam = {
+      teamName: '',
+      sport: '',
+      firstName: $scope.firstName,
+      lastName: $scope.lastName,
+      emailAddress: $scope.emailAddress,
+      password: '',
+      teamId: 0
+    }
+
+    $scope.submitCoach = function() {
+      $localStorage.$reset();
+      $http({
+        url: 'http://localhost:3000/newTeam',
+        method: 'POST',
+        data: {
+          teamName: $scope.coachAndTeam.teamName,
+          sport: $scope.coachAndTeam.sport
+        }
+      }).then( response => {
+        $scope.coachAndTeam.teamId = response.data.id;
+        $http({
+          url: 'http://localhost:3000/newCoach',
+          method: 'POST',
+          data: {
+            firstName: $scope.coachAndTeam.firstName,
+            lastName: $scope.coachAndTeam.lastName,
+            emailAddress: $scope.coachAndTeam.emailAddress,
+            password: $scope.coachAndTeam.password,
+            teamId: $scope.coachAndTeam.teamId
+          }
+        }).then(response => {
+          console.log(response);
+        })
+      })
+
+    }
+  });
