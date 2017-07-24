15f7103c9fb5a07904df2ac5330336152e1c43db
Heathcliff <dennis.nathan@gmail.com>
2017-07-24 14:32:34 -0600

    Added form to /moreinfo, created functions to send roster to backend

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -19,16 +19,6 @@ angular.module('lifesparqApp')
         pic: 'http://lorempixel.com/152/152/sports/3',
         name: 'Eric',
         info: 'Our fearless leader, carrying us forward into the unknown.'
-      },
-      {
-        pic: 'http://lorempixel.com/152/152/sports/5',
-        name: 'Breeze',
-        info: 'Breeze is a dietician extraordinaire. There is nothing that she doesn\'t know about nutrition, and nobody in the entire world who knows more than her.'
-      },
-      {
-        pic: 'http://lorempixel.com/152/152/sports/1',
-        name: 'Mark',
-        info: 'Mark has potential, probably.'
       }
     ]
   });
