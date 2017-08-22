7cc5ac2bc3caee6df6b68118b03d1de53b352c8d
Ryan Harings <ryanharings@hotmail.com>
2017-08-22 16:38:48 -0500

    added sendFeedback route

--- app/scripts/controllers/feedback.js
+++ app/scripts/controllers/feedback.js
@@ -1,96 +1,111 @@
-'use strict';
+(function() {
+  'use strict';
 
-angular.module('lifesparqApp')
-  .controller('feedbackCtrl', function ($scope, $http, $location, $localStorage, $cookies) {
+  angular.module('lifesparqApp')
+    // .service('feedbackService')
+    .controller('feedbackCtrl', function ($scope, $http, $location, $localStorage, $cookies, feedbackService) {
 
-    $scope.master = {};
+      $scope.master = {};
 
-    $scope.update = function(user) {
-      $scope.master = angular.copy(user);
-    };
+      $scope.update = function(user) {
+        $scope.master = angular.copy(user);
+      };
 
-    $scope.reset = function() {
-      $scope.user = angular.copy($scope.master);
-    };
+      $scope.reset = function() {
+        $scope.user = angular.copy($scope.master);
+      };
 
-    $scope.submit = function() {
-      if ($scope.user){
-        console.log($scope.user);
-      }
-    };
+      $scope.submit = function() {
+        if ($scope.user){
+          console.log($scope.user);
+        }
+      };
 
-    $scope.reset();
+      $scope.reset();
 
+      $scope.submitFeedback = function() {
+        feedbackService.sendFeed($scope.user)
+        .then(({ data }) => {
+          console.log(data, 'controller');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+      };
 
-  //   $scope.showTeamSignup = function(ev) {
-  //     $mdDialog.show({
-  //       controller: userController,
-  //       templateUrl: 'signupFormTeam.tmpl.html',
-  //       parent: angular.element(document.body),
-  //       targetEvent: ev,
-  //       clickOutsideToClose: true
-  //     });
-  //   }
-  //
-  //   $localStorage.$reset();
-  //
-  // $scope.user = {
-  //   firstName: '',
-  //   lastName: '',
-  //   emailAddress: '',
-  //   password: ''
-  // };
-  //
-  // $scope.error = '';
+      // $scope.feedback();
 
-  function userController($scope) {
-
-    // $scope.feedbackUser = function() {
-    //   $http({
-    //     url: 'http://localhost:3000/compare',
-    //     method: 'POST',
-    //     data: {
-    //       emailAddress: $scope.user.emailAddress,
-    //       password: $scope.user.password
-    //     }
-    //   }).then(response => {
-    //     if (response.data) {
-    //       var date = new Date();
-    //       date.setTime(date.getTime()+((60*1000)*120));
-    //       $cookies.put('authToken', response.data, {'expires': date});
-    //       $location.path('/profile');
-    //     }
-    //     // else if (response.data.success) {
-    //     //   $cookies.put('test-cookie-defaults', 'worked');
-    //     //   $localStorage.firstName = response.data.firstName;
-    //     //   $localStorage.lastName = response.data.lastName;
-    //     //   $localStorage.emailAddress = response.data.emailAddress;
-    //     //   $localStorage.teamName = response.data.teamName;
-    //     //   $localStorage.profilePicture = response.data.profilePicture;
-    //     //   $mdDialog.hide();
-    //     //   $location.path('/profile');
-    //     // } else {
-    //     //   console.log('That feedback didn\'t work');
-    //     // }
-    //     console.log(response);
-    //   })
-    // }
-
-    // $scope.submitUser = function() {
-    //   $localStorage.firstName = $scope.user.firstName;
-    //   $localStorage.lastName = $scope.user.lastName;
-    //   $localStorage.emailAddress = $scope.user.emailAddress;
-    //   $mdDialog.hide();
-    //   $location.path('/moreinfosingle');
-    // }
+    //   $scope.showTeamSignup = function(ev) {
+    //     $mdDialog.show({
+    //       controller: userController,
+    //       templateUrl: 'signupFormTeam.tmpl.html',
+    //       parent: angular.element(document.body),
+    //       targetEvent: ev,
+    //       clickOutsideToClose: true
+    //     });
+    //   }
     //
-    // $scope.submitCoach = function() {
-    //   $localStorage.firstName = $scope.user.firstName;
-    //   $localStorage.lastName = $scope.user.lastName;
-    //   $localStorage.emailAddress = $scope.user.emailAddress;
-    //   $mdDialog.hide();
-    //   $location.path('/moreinfocoach');
-    // }
+    //   $localStorage.$reset();
+    //
+    // $scope.user = {
+    //   firstName: '',
+    //   lastName: '',
+    //   emailAddress: '',
+    //   password: ''
+    // };
+    //
+    // $scope.error = '';
 
-  }
-});
+    function userController($scope) {
+
+      // $scope.feedbackUser = function() {
+      //   $http({
+      //     url: 'http://localhost:3000/compare',
+      //     method: 'POST',
+      //     data: {
+      //       emailAddress: $scope.user.emailAddress,
+      //       password: $scope.user.password
+      //     }
+      //   }).then(response => {
+      //     if (response.data) {
+      //       var date = new Date();
+      //       date.setTime(date.getTime()+((60*1000)*120));
+      //       $cookies.put('authToken', response.data, {'expires': date});
+      //       $location.path('/profile');
+      //     }
+      //     // else if (response.data.success) {
+      //     //   $cookies.put('test-cookie-defaults', 'worked');
+      //     //   $localStorage.firstName = response.data.firstName;
+      //     //   $localStorage.lastName = response.data.lastName;
+      //     //   $localStorage.emailAddress = response.data.emailAddress;
+      //     //   $localStorage.teamName = response.data.teamName;
+      //     //   $localStorage.profilePicture = response.data.profilePicture;
+      //     //   $mdDialog.hide();
+      //     //   $location.path('/profile');
+      //     // } else {
+      //     //   console.log('That feedback didn\'t work');
+      //     // }
+      //     console.log(response);
+      //   })
+      // }
+
+      // $scope.submitUser = function() {
+      //   $localStorage.firstName = $scope.user.firstName;
+      //   $localStorage.lastName = $scope.user.lastName;
+      //   $localStorage.emailAddress = $scope.user.emailAddress;
+      //   $mdDialog.hide();
+      //   $location.path('/moreinfosingle');
+      // }
+      //
+      // $scope.submitCoach = function() {
+      //   $localStorage.firstName = $scope.user.firstName;
+      //   $localStorage.lastName = $scope.user.lastName;
+      //   $localStorage.emailAddress = $scope.user.emailAddress;
+      //   $mdDialog.hide();
+      //   $location.path('/moreinfocoach');
+      // }
+
+    }
+  });
+
+}());
