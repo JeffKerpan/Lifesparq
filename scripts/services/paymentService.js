79df2e9ae5db848e3da1fe3ce8b93f5f4dcc0272
Ryan Harings <ryanharings@hotmail.com>
2017-09-05 01:10:11 -0600

    added stripe front-end

--- scripts/services/paymentService.js
+++ scripts/services/paymentService.js
@@ -4,27 +4,89 @@
   angular.module('lifesparqApp')
     .service('paymentService', function($http, $window, $cookies) {
 
-      const feedbackUrl = 'http://localhost:3000/';
+      var testKey = 'pk_test_jMkpcoWvYd8X52zgFStrIriZ';
+      var prodKey = '';
 
-      this.sendFeed = (feedback) => {
-        if (feedback.name && feedback.message) {
-          var token = $cookies.get('Authorization');
-          $http.defaults.headers.common.Authorization = `Bearer ${token}`;
-          return $http.post(feedbackUrl + 'feedback',
-          {
-            name: feedback.name,
-            message: feedback.message
-          })
-          // .then(feedResponse => {
-          //   console.log(feedResponse, 'service');
-          // })
-          .catch(err => {
-            console.log(err);
+      var stripe = Stripe(testKey);
+      var elements = stripe.elements();
+
+      var style;
+      var card;
+
+      this.createStripeForm = () => {
+        style = {
+          base: {
+            // color: '#32325d',
+            color: 'blue',
+            lineHeight: '24px',
+            fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+            fontSmoothing: 'antialiased',
+            fontSize: '16px',
+            '::placeholder': {
+              color: '#aab7c4'
+            }
+          },
+          invalid: {
+            color: '#fa755a',
+            iconColor: '#fa755a'
+          }
+        };
+        card = elements.create('card', {style: style});
+
+      }
+
+      this.createStripeForm();
+
+      this.loadStripeForm = () => {
+        card.mount('#card-element');
+        card.addEventListener('change', function(event) {
+          var displayError = document.getElementById('card-errors');
+          if (event.error) {
+            displayError.textContent = event.error.message;
+          } else {
+            displayError.textContent = '';
+          }
+        });
+
+        // Handle form submission
+        var form = document.getElementById('payment-form');
+        form.addEventListener('submit', function(event) {
+          event.preventDefault();
+
+          stripe.createToken(card).then(function(result) {
+            if (result.error) {
+              // Inform the user if there was an error
+              var errorElement = document.getElementById('card-errors');
+              errorElement.textContent = result.error.message;
+            } else {
+              // Send the token to your server
+              stripeTokenHandler(result.token);
+            }
           });
-        } else {
-          console.log('incomplete');
-        }
-      };
+        });
+      }
+
+      // const feedbackUrl = 'http://localhost:3000/';
+      //
+      // this.sendFeed = (feedback) => {
+      //   if (feedback.name && feedback.message) {
+      //     var token = $cookies.get('Authorization');
+      //     $http.defaults.headers.common.Authorization = `Bearer ${token}`;
+      //     return $http.post(feedbackUrl + 'feedback',
+      //     {
+      //       name: feedback.name,
+      //       message: feedback.message
+      //     })
+      //     // .then(feedResponse => {
+      //     //   console.log(feedResponse, 'service');
+      //     // })
+      //     .catch(err => {
+      //       console.log(err);
+      //     });
+      //   } else {
+      //     console.log('incomplete');
+      //   }
+      // };
     }
   );
 
