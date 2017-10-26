c9b8954721171a08bf996175f1470c1b7bc29c5a
Ryan Harings <ryanharings@hotmail.com>
2017-10-25 21:12:27 -0500

    added tab templates to profile page

--- scripts/controllers/profile.js
+++ scripts/controllers/profile.js
@@ -1,20 +1,7 @@
 'use strict';
 
 angular.module('lifesparqApp')
-  .controller('profileCtrl', function ($scope, $mdSidenav, $timeout, $log, $localStorage, $cookies, $location, $http) {
-    $scope.toggleLeft = buildDelayedToggler('right');
-    $scope.toggleRight = buildToggler('right');
-    $scope.isOpenRight = function(){
-      return $mdSidenav('right').isOpen();
-    };
-
-    $scope.authenticatedUser = {
-      firstName: '',
-      lastName: '',
-      emailAddress: '',
-      teamName: '',
-      profilePicture: ''
-    }
+  .controller('profileCtrl', function ($scope, $mdSidenav, $timeout, $log, $localStorage, $cookies, $location, $http, $mdDialog) {
 
     function getUserInfo() {
       var token = $cookies.get('Authorization');
@@ -39,63 +26,64 @@ angular.module('lifesparqApp')
     // $scope.testing = Base64.encode(firstName);
     // console.log($scope.testing);
 
-    function debounce(func, wait, context) {
-      var timer;
-
-      return function debounced() {
-        var context = $scope,
-            args = Array.prototype.slice.call(arguments);
-        $timeout.cancel(timer);
-        timer = $timeout(function() {
-          timer = undefined;
-          func.apply(context, args);
-        }, wait || 10);
-      };
-    }
+    // function debounce(func, wait, context) {
+    //   var timer;
+    //
+    //   return function debounced() {
+    //     var context = $scope,
+    //         args = Array.prototype.slice.call(arguments);
+    //     $timeout.cancel(timer);
+    //     timer = $timeout(function() {
+    //       timer = undefined;
+    //       func.apply(context, args);
+    //     }, wait || 10);
+    //   };
+    // }
 
     /**
      * Build handler to open/close a SideNav; when animation finishes
      * report completion in console
      */
-    function buildDelayedToggler(navID) {
-      return debounce(function() {
-        // Component lookup should always be available since we are not using `ng-if`
-        $mdSidenav(navID)
-          .toggle()
-          .then(function () {
-            $log.debug("toggle " + navID + " is done");
-          });
-      }, 200);
-    }
-
-    function buildToggler(navID) {
-      return function() {
-        // Component lookup should always be available since we are not using `ng-if`
-        $mdSidenav(navID)
-          .toggle()
-          .then(function () {
-            $log.debug("toggle " + navID + " is done");
-          });
-      };
-    }
+    // function buildDelayedToggler(navID) {
+    //   return debounce(function() {
+    //     // Component lookup should always be available since we are not using `ng-if`
+    //     $mdSidenav(navID)
+    //       .toggle()
+    //       .then(function () {
+    //         $log.debug("toggle " + navID + " is done");
+    //       });
+    //   }, 200);
+    // }
+    //
+    // function buildToggler(navID) {
+    //   return function() {
+    //     // Component lookup should always be available since we are not using `ng-if`
+    //     $mdSidenav(navID)
+    //       .toggle()
+    //       .then(function () {
+    //         $log.debug("toggle " + navID + " is done");
+    //       });
+    //   };
+    // }
 
 })
-  .controller('leftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
-    $scope.close = function () {
-      // Component lookup should always be available since we are not using `ng-if`
-      $mdSidenav('left').close()
-        .then(function () {
-          $log.debug("close LEFT is done");
-        });
-
-    };
-  })
-  .controller('rightCtrl', function ($scope, $timeout, $mdSidenav, $log) {
-    $scope.close = function () {
-      // Component lookup should always be available since we are not using `ng-if`
-      $mdSidenav('right').close()
-        .then(function () {
-          $log.debug("close RIGHT is done");
-        });
-    };
-});
+// .controller('leftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+  // $scope.close = function () {
+//     // Component lookup should always be available since we are not using `ng-if`
+//     $mdSidenav('left').close()
+//       .then(function () {
+//         $log.debug("close LEFT is done");
+//       });
+//
+//   };
+// })
+// .controller('rightCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+//   $scope.close = function () {
+//     // Component lookup should always be available since we are not using `ng-if`
+//     $mdSidenav('right').close()
+//       .then(function () {
+//         $log.debug("close RIGHT is done");
+//       });
+//   };
+// })
+;
