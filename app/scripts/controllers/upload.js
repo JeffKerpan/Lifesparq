a4201d31a006b4f2504eebd29def4d1c5f1c4e1f
Heathcliff <dennis.nathan@gmail.com>
2017-10-30 15:33:26 -0600

    Built upload page, added content to main page, added background to login page

--- app/scripts/controllers/upload.js
+++ app/scripts/controllers/upload.js
@@ -1,56 +1,97 @@
 'use strict';
 
 angular.module('lifesparqApp')
-  .controller('uploadCtrl', function ($scope, $http, $localStorage, $location) {
+  .controller('uploadCtrl', function ($scope, $http, $localStorage, $location, $cookies) {
 
-    $scope.videosForUpload = [];
+    $scope.allVideos = [];
+    $scope.next;
+    $scope.nextUrl;
+    $scope.previousUrl;
+    $scope.showUploadInput = false;
+    $scope.currentTitle = '';
+    $scope.videoDescription = '';
+    $scope.file;
+    $scope.form;
+    $scope.selectedVideo;
+    const token = $cookies.get('Authorization');
 
-    $scope.addVideoToUpload = function () {
-      var video = document.getElementById('uploadVideo').files;
-      $scope.videosForUpload.push(video);
+    $scope.verifyAccess = function () {
+      $http.defaults.headers.common.Authorization = `Bearer ${token}`;
+      return $http({
+        url: 'http://localhost:3000/super/verify',
+        method: 'POST'
+      })
+      .then(result => {
+        if (result.data === true) {
+          console.log('true');
+          $scope.getAllVideos();
+        } else {
+          $location.path('/');
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        $location.path('/');
+      });
     }
 
-    $scope.saveVideo = function () {
-      const files = document.getElementById('uploadVideo').files;
-      const file = files[0];
-      if(!file) {
-        console.log('No file found');
-        return;
+    $scope.verifyAccess();
+
+    $scope.getAllVideos = function(url) {
+      let getUrl = '';
+      if (url) {
+        getUrl = 'customvideos';
+        $http.defaults.headers.common.path = url;
+      } else {
+        getUrl = 'allvideos';
       }
-      $scope.getSignedRequest(file);
+
+      $http.defaults.headers.common.Authorization = `Bearer ${token}`;
+
+      $http.get(`http://localhost:3000/sprout/${getUrl}`)
+        .then(result => {
+          console.log(result);
+          if (result.data.next_page) {
+            $scope.next = true;
+            $scope.nextUrl = result.data.next_page.split('/')[4];
+            console.log($scope.nextUrl);
+          } else {
+            $scope.next = false;
+          }
+          if (result.data.previous_page) {
+            $scope.previous = true;
+            $scope.previousUrl = result.data.previous_page.split('/')[4];
+          } else {
+            $scope.previous = false;
+          }
+          $scope.allVideos = result.data.videos;
+        }).catch(err => {
+          console.log(err);
+        })
     }
 
-    $scope.getSignedRequest = function (file) {
-      const xhr = new XMLHttpRequest();
-      xhr.open('GET', `https://stormy-springs-94108.herokuapp.com/super/sign-s3?file-name=${file.name}&file-type=${file.type}`);
-      xhr.onreadystatechange = () => {
-        if(xhr.readyState === 4) {
-          if(xhr.status === 200) {
-            const response = JSON.parse(xhr.responseText);
-            $scope.uploadFile(file, response.signedRequest, response.url);
-          }
-          else {
-            alert('Could not get signed URL.');
-          }
-        }
-      };
-      xhr.send();
+    $scope.showUpload = function(video) {
+      $scope.selectedVideo = video;
+      $scope.showUploadInput = true;
+      $scope.currentTitle = video.title;
     }
 
-    $scope.uploadFile = function (file, signedRequest, url) {
-      const xhr = new XMLHttpRequest();
-      xhr.open('PUT', signedRequest);
-      xhr.onreadystatechange = () => {
-        if(xhr.readyState === 4) {
-          if(xhr.status === 200) {
-            document.getElementById('preview').src = url;
-            document.getElementById('avatar-url').value = url;
-          }
-          else {
-            alert('Could not upload file.');
-          }
+    $scope.postFile = function() {
+      $scope.file = document.getElementById('file').files[0];
+      $scope.videoDescription = document.getElementById('videoDescription').value;
+      $scope.form = new FormData($scope.file);
+      $http({
+        url: 'http://localhost:3000/cloudinary',
+        method: 'POST',
+        data: {
+          video: $scope.selectedVideo,
+          videoDescription: $scope.videoDescription
         }
-      };
-      xhr.send(file);
+      }).then(response => {
+        console.log(response);
+      }).catch(err => {
+        console.log(err);
+      })
     }
+
   });
